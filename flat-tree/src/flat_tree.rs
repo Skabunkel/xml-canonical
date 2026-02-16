037f2fd075a2e661e6a165ca21e645a967fb5be6
SkaBunkel <skabunkel@users.noreply.github.com>
2026-02-16 21:53:33 +0100

    chore: added more logic

--- flat-tree/src/flat_tree.rs
+++ flat-tree/src/flat_tree.rs
@@ -10,8 +10,8 @@ pub type Depth = u8;
 pub struct Node(usize);
 
 /// A simple tuple for a node and depth pair.
-pub type FlatNode = (XNode, u8);
-pub type FlatNodeRef<'a> = (&'a XNode, &'a u8);
+pub type FlatNode = (XNode, Depth);
+pub type FlatNodeRef<'a> = (&'a XNode, &'a Depth);
 
 #[derive(Default)]
 pub struct FlatTree {
@@ -56,5 +56,53 @@ impl FlatTree {
       nodes: &self.nodes[range.clone()],
       depth: &self.depth[range],
     }
+  }
+
+  pub fn push(&mut self, node: FlatNode) {
+    self.nodes.push(node.0);
+    self.depth.push(node.1);
+  }
+
+  pub fn pop(&mut self) -> Option<FlatNode> {
+    let node = self.nodes.pop()?;
+    let depth = self.depth.pop()?;
+
+    Some((node, depth))
+  }
+
+  pub fn insert(&mut self, index: usize, node: FlatNode) {
+    self.nodes.insert(index, node.0);
+    self.depth.insert(index, node.1);
+  }
+
+  pub fn remove(&mut self, index: usize) -> FlatNode {
+    let node = self.nodes.remove(index);
+    let depth = self.depth.remove(index);
+
+    (node, depth)
+  }
+
+  pub fn to_node_builder(&self) -> Node {
+    let len = self.nodes.len();
+
+    Node(len)
+  }
+
+  pub fn is_empty(&self) -> bool {
+    self.nodes.is_empty()
+  }
+
+  pub fn get(&self, index: usize) -> Option<FlatNodeRef<'_>> {
+    if self.nodes.is_empty() {
+      return None;
+    }
+    let depth = self.depth.get(index)?;
+    let node = self.nodes.get(index)?;
+
+    Some((node, depth))
+  }
+
+  pub fn len(&self) -> usize {
+    self.nodes.len()
   }
 }
