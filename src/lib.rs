730b0f5191f21cbe1c6c2e1195583c5046d8cd9a
SkaBunkel <skabunkel@users.noreply.github.com>
2026-03-08 09:09:05 +0100

    chore: forgot about lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub use flat_tree::{
-  elements::XAttribute, elements::XNamespace, elements::XNode, flat_tree::FlatNode,
+  elements::XDecorator, elements::XNode, flat_tree::FlatNode,
   flat_tree::FlatNodeMutRef, flat_tree::FlatNodeRef, flat_tree::FlatTree,
   flat_tree_slice::FlatTreeSlice,
 };
