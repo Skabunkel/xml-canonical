fb292fcf845ec867bf97b5f44ec47f93ceb0a16d
SkaBunkel <skabunkel@users.noreply.github.com>
2026-03-08 10:14:02 +0100

    chore: updated mod file

--- canonicalizer/src/rules/mod.rs
+++ canonicalizer/src/rules/mod.rs
@@ -1,17 +1,17 @@
-mod strip_declaration;
-mod strip_comments;
+mod expand_empty_elements;
 mod normalize_line_endings;
-mod sort_namespaces;
 mod sort_attributes;
+mod sort_namespaces;
+mod strip_comments;
+mod strip_declaration;
 mod strip_redundant_namespaces;
 mod visibly_utilized_namespaces;
-mod expand_empty_elements;
 
-pub use strip_declaration::StripDeclaration;
-pub use strip_comments::StripComments;
+pub use expand_empty_elements::ExpandEmptyElements;
 pub use normalize_line_endings::NormalizeLineEndings;
-pub use sort_namespaces::SortNamespaces;
 pub use sort_attributes::SortAttributes;
+pub use sort_namespaces::SortNamespaces;
+pub use strip_comments::StripComments;
+pub use strip_declaration::StripDeclaration;
 pub use strip_redundant_namespaces::StripRedundantNamespaces;
 pub use visibly_utilized_namespaces::VisiblyUtilizedNamespaces;
-pub use expand_empty_elements::ExpandEmptyElements;
